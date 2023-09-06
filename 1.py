7a85d8fb0903eefda13557059a619b5ea70e0b28
steeluv <enikeeva.violeta@mail.ru>
2023-09-06 14:05:48 +0300

    hw

--- 1.py
+++
@@ -1,9 +0,0 @@
-bta = []
-eta = []
-lta = []
-for i in list_of_nums:
-    if i > arg
-        bta.append(i)
-    if i < eta.append(i)
-        lta.append(i)
-    else:
---
+++ 2 пара.py
@@ -0,0 +1,5 @@
+
+example = ['egor', 'oleg', 'katya', 'sonya']
+n1,n2,_, n3 = example
+print(n1, n2, n3)
+
