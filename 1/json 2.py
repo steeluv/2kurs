28fc2f97b43f02759211f8113b5e3942ef818447
steeluv <enikeeva.violeta@mail.ru>
2023-09-14 20:34:45 +0300

    json

---
+++ 1/json 2.py
@@ -0,0 +1,18 @@
+"""
+Сохраните данные из списка в json файл(Имя файла - ваша фамилия и номер задания) с отступом 4, формата:
+name: ***
+age: ***
+countries: ***
+"""
+task = ["oleg",24,["Belarus","Russia"]]
+
+data = {'name': task[0],
+        'age': task[1],
+        'countries': task[2][0]}
+
+import json
+
+with open('steeluv.json', 'w') as json_file:
+
+    json.dump(data, json_file, indent=4)
+
